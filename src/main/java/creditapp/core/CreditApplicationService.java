6d6192061c52df9fc7110fa2f535c030e13873fa
Daniel <kryszkiewicz.daniel1@gmail.com>
2020-07-12 21:14:00 +0200

    update

--- src/main/java/creditapp/core/CreditApplicationService.java
+++ src/main/java/creditapp/core/CreditApplicationService.java
@@ -2,6 +2,9 @@ package creditapp.core;
 
 public class CreditApplicationService {
     String getDecision(Person person){
+        PersonScoringCalculator scoringCalculator = new PersonScoringCalculator();
+        String decision = scoringCalculator.calculate(person)   <300 ? "Decision is negative" : "Decision is positive";
+        return decision;
 
     }
 }
